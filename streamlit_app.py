2dd9b67bb03f9355f542892b8bd1a6e05a6c9db6
subhashsnowflakecertification <136854378+subhashsnowflakecertification@users.noreply.github.com>
2023-06-16 16:17:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,13 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner') 
 
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard Boild free Range Eggs')
+streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boild free Range Eggs')
+streamlit.text('🥑🍞 Avacado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
